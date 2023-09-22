1352fc8caaed86f90d3a03b3e02f44a585f21598
TheDarkSide1992 <Jenscp171@gmail.com>
2023-09-22 10:28:32 +0200

    init commit of sln

---
+++ console/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+Console.WriteLine("Hello :--) Please enter the text you want to see encrypted");
+string? enteredText = Console.ReadLine();
+
+
+Console.WriteLine("toHexString");
+
+byte[] binary = Encoding.UTF8.GetBytes(enteredText);
+
+SoutEcrypted(binary);
+
+string text = Encoding.UTF8.GetString(binary);
+
+SoutDecrypted(text);
+
+
+
+
+//--------------------
+
+void SoutEcrypted(byte[] encrypted)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.Write("Text Encrypted: ");
+    foreach (var item in encrypted) {
+        Console.Write(item);
+    }
+    Console.WriteLine();
+    Console.ResetColor();
+}
+
+void SoutDecrypted(string decrypted)
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Text Decrypted: " + decrypted);
+    Console.ResetColor();
+
+}
